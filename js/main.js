6bde03b4ae47b5e5de1de9c0926567000b3a4bfa
Angus Lei <alei@sfu.ca>
2019-07-06 15:38:50 -0700

    Add iteration of array of designer for hover functionality, use for mouse exit

--- js/main.js
+++ js/main.js
@@ -1,14 +1,26 @@
 $(document).ready(function(){
   console.log("WE READY");
-  var florisDot = $("#pepeHoverDot");
+  var designerDots = $(".designer-dot").get();
   var startingBG = "linear-gradient(120deg, #d4fc79 0%, #96e6a1 100%)"
+  console.log(designerDots);
+  //TODO: iterate over designerDots and do the same functions for each
 
-  florisDot.mouseenter(function(){
-      console.log("entered");
-      $("html").css("background-image", "linear-gradient(120deg, #f093fb 0%, #f5576c 100%)");
-  });
-  florisDot.mouseout(function(){
-      console.log("exit");
-      $("html").css("background-image", startingBG);
+  function changeBackground(){
+    console.log("entered");
+    $("html").css("background-image", "linear-gradient(120deg, #f093fb 0%, #f5576c 100%)");
+  }
+
+  function revertBackground(){
+    console.log("exit");
+    $("html").css("background-image", startingBG);
+  }
+
+  designerDots.forEach(function(designer){
+      $(designer).mouseenter(function(){
+        changeBackground();
+      })
+      $(designer).mouseout(function(){
+        revertBackground();
+      })
   });
 });
