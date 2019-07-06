0854d1514f107ad5007c395732c5e60244ad0706
Angus Lei <alei@sfu.ca>
2019-07-06 14:44:51 -0700

    Add hover to change background functions with duel images.

--- js/main.js
+++ js/main.js
@@ -1,4 +1,14 @@
 $(document).ready(function(){
   console.log("WE READY");
-  // var florisDot = $(document).getElementById('designerName-hover');
+  var florisDot = $("#pepeHoverDot");
+  var startingBG = "linear-gradient(120deg, #d4fc79 0%, #96e6a1 100%)"
+
+  florisDot.mouseenter(function(){
+      console.log("entered");
+      $("html").css("background-image", "linear-gradient(120deg, #f093fb 0%, #f5576c 100%)");
+  });
+  florisDot.mouseout(function(){
+      console.log("exit");
+      $("html").css("background-image", startingBG);
+  });
 });
