3ba8baf1c79e1b2f0a66eb8f6908a6c4a1000f70
Angus Lei <alei@sfu.ca>
2019-07-06 19:58:30 -0700

    background video on dot hover behaviour functional

--- js/main.js
+++ js/main.js
@@ -2,23 +2,21 @@ $(document).ready(function(){
   console.log("WE READY");
   var designerDots = $(".designer-dot").get();
   var startingBG = "linear-gradient(120deg, #d4fc79 0%, #96e6a1 100%)"
+  var bgVideo = $("#designer-preview-vid");
 
   function changeBackground(){
-    /*TODO: define how each background video will change depending on which designer's dot it is? Is this worth doing over an iteration? Each individual designer will have a different background, might as well make new functions?
-    PSUEDOCODE
-      pass the object that's calling this functions as a parameter
-      IF this object's ID attribute corresponds to a specific designer
-      then change background to corresponding video
-    */
-    $("html").css("background-image", "linear-gradient(120deg, #f093fb 0%, #f5576c 100%)");
+    bgVideo.css("display", "block");
   }
   function revertBackground(){
+    bgVideo.css("display", "none");
     $("html").css("background-image", startingBG);
   }
 
+  console.log(bgVideo);
+
   designerDots.forEach(function(designer){
       $(designer).mouseenter(function(){
-        changeBackground();
+        changeBackground(designer);
       })
       $(designer).mouseout(function(){
         revertBackground();
