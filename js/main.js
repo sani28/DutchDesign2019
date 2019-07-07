ab4b2e57db4cfbd0919d0ec9d25e133e5ca824ea
Angus Lei <alei@sfu.ca>
2019-07-06 19:10:55 -0700

    add main.js file and hover interactions iterating over designer dots

--- js/main.js
+++ js/main.js
@@ -2,16 +2,19 @@ $(document).ready(function(){
   console.log("WE READY");
   var designerDots = $(".designer-dot").get();
   var startingBG = "linear-gradient(120deg, #d4fc79 0%, #96e6a1 100%)"
-  console.log(designerDots);
-  //TODO: iterate over designerDots and do the same functions for each
 
   function changeBackground(){
-    console.log("entered");
+    /*TODO: define how each background video will change depending on which designer's dot it is? Is this worth doing over an iteration? Each individual designer will have a different background, might as well make new functions?
+    PSUEDOCODE
+      pass the object that's calling this functions as a parameter
+      IF this object's ID attribute corresponds to a specific designer
+      then change background to corresponding video
+
+    */
     $("html").css("background-image", "linear-gradient(120deg, #f093fb 0%, #f5576c 100%)");
   }
 
   function revertBackground(){
-    console.log("exit");
     $("html").css("background-image", startingBG);
   }
 
