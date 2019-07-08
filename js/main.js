41983dc17b7bf2a908040a95d52d789559e64fa8
Angus Lei <alei@sfu.ca>
2019-07-07 21:36:49 -0700

    Add a switch to decide which preview to play, add more cases later

--- js/main.js
+++ js/main.js
@@ -1,22 +1,33 @@
 $(document).ready(function(){
 
-
   var designerDots = $(".designer-dot").get();
   var startingBG = "linear-gradient(120deg, #d4fc79 0%, #96e6a1 100%)"
-  var bgVideo = $("#designer-preview-vid");
+  var previewVideos = $(".bg-preview-video").get();
 
-  function changeBackground(){
-    /*TODO: take a parameter changeBackground(dot)
-    gets the ID of the "dot" argument, and gives a corresponding background video to display*/
-    bgVideo.css("display", "block");
+  function playPreviewVideo(videoID){
+    var currentVideo = videoID;
+    switch(videoID){ //takes a string from the index html
+      case "lexDot":
+        $("#lex-preview-vid").css("display", "block");
+        break;
+      case "valentineDot":
+        $("#valentine-preview-vid").css("display", "block");
+        break;
+    }
   }
+
   function revertBackground(){
-    bgVideo.css("display", "none");
+    hidePreviewVideos();
     $("html").css("background-image", startingBG);
+  }
+  function hidePreviewVideos(){
+    for(var i=0, len=previewVideos.length; i< len; i++){
+      $(previewVideos[i]).css("display", "none");
+    }
   }
   // is this better: https://gomakethings.com/listening-for-click-events-with-vanilla-javascript/
 
-  //TODO: I should be able to refacto this to reduce code length by 1/2
+  //TODO: I should be able to refactor this to reduce code length by 1/2
   for (var i=0, len=designerDots.length; i < len; i++){
     //passes designerDots as an array into a function
     $(designerDots[i]).mouseenter(function(){
@@ -26,7 +37,7 @@ $(document).ready(function(){
           $(designerDots[j]).addClass("dot-unhovered");
         };
       }
-      changeBackground();
+      playPreviewVideo(currentDesigner.id);
     });
     $(designerDots[i]).mouseout(function(){
       var currentDesigner = this;
@@ -38,7 +49,6 @@ $(document).ready(function(){
       revertBackground();
     });
   }
-
 
 
 }); //DOCREADY
