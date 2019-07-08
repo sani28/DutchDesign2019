520a6eee5cbde2b0fc308747716c6609e16a527c
Angus Lei <alei@sfu.ca>
2019-07-07 20:23:18 -0700

    Add functionality for hovered dots to change style of non-hovered dots

--- js/main.js
+++ js/main.js
@@ -1,5 +1,6 @@
 $(document).ready(function(){
-  console.log("WE READY");
+
+
   var designerDots = $(".designer-dot").get();
   var startingBG = "linear-gradient(120deg, #d4fc79 0%, #96e6a1 100%)"
   var bgVideo = $("#designer-preview-vid");
@@ -11,13 +12,30 @@ $(document).ready(function(){
     bgVideo.css("display", "none");
     $("html").css("background-image", startingBG);
   }
+  // is this better: https://gomakethings.com/listening-for-click-events-with-vanilla-javascript/
 
   for (var i=0, len=designerDots.length; i < len; i++){
+    //passes designerDots as an array into a function
     $(designerDots[i]).mouseenter(function(){
+      var current = this;
+      for(var j=0, len=designerDots.length; j <len; j++){
+        if(designerDots[j]!=current){
+          $(designerDots[j]).addClass("dot-unhovered");
+        };
+      }
       changeBackground();
     });
     $(designerDots[i]).mouseout(function(){
+      var current = this;
+      for(var j=0, len=designerDots.length; j <len; j++){
+        if(designerDots[j]!=current){
+          $(designerDots[j]).removeClass("dot-unhovered");
+        };
+      }
       revertBackground();
-    })
+    });
   }
-});
+
+
+
+}); //DOCREADY
