6474163487c5c01d752ab113ef3ef41f918c9f89
Angus Lei <alei@sfu.ca>
2019-07-08 16:23:32 -0700

    Add comments and clean up indentation with beautify

--- js/main.js
+++ js/main.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:dd96f080d86cecab195e2c4634b2d3308d32dbb05afc24d0e7620ab79f0dc3e9
-size 1824
+oid sha256:7b07f7d19117100c00c5bd04ee642bb311364eeab910d321c37e7bd00e64edab
+size 1736
