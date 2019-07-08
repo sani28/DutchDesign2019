892a123452c66447e280803095ec3131e7965da9
Angus Lei <alei@sfu.ca>
2019-07-08 16:37:46 -0700

    Hide experience dots when mouse on designer dots, revert when mouse off

--- js/main.js
+++ js/main.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:7b07f7d19117100c00c5bd04ee642bb311364eeab910d321c37e7bd00e64edab
-size 1736
+oid sha256:a11b2df8ce13d44e4c82968cda9f453b52103d1f756a1575e95637278b77a4b5
+size 2149
