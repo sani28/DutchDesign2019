74c23ad9d0081d0baf959b4bf67411e8d573125d
Angus Lei <alei@sfu.ca>
2019-07-07 21:13:59 -0700

    Include TODO: comments and renamed some variables for clarity

--- js/main.js
+++ js/main.js
@@ -6,6 +6,8 @@ $(document).ready(function(){
   var bgVideo = $("#designer-preview-vid");
 
   function changeBackground(){
+    /*TODO: take a parameter changeBackground(dot)
+    gets the ID of the "dot" argument, and gives a corresponding background video to display*/
     bgVideo.css("display", "block");
   }
   function revertBackground(){
@@ -14,21 +16,22 @@ $(document).ready(function(){
   }
   // is this better: https://gomakethings.com/listening-for-click-events-with-vanilla-javascript/
 
+  //TODO: I should be able to refacto this to reduce code length by 1/2
   for (var i=0, len=designerDots.length; i < len; i++){
     //passes designerDots as an array into a function
     $(designerDots[i]).mouseenter(function(){
-      var current = this;
+      var currentDesigner = this;
       for(var j=0, len=designerDots.length; j <len; j++){
-        if(designerDots[j]!=current){
+        if(designerDots[j]!=currentDesigner){
           $(designerDots[j]).addClass("dot-unhovered");
         };
       }
       changeBackground();
     });
     $(designerDots[i]).mouseout(function(){
-      var current = this;
+      var currentDesigner = this;
       for(var j=0, len=designerDots.length; j <len; j++){
-        if(designerDots[j]!=current){
+        if(designerDots[j]!=currentDesigner){
           $(designerDots[j]).removeClass("dot-unhovered");
         };
       }
