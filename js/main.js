65f1a2dd02df29b69195849ec95e46adb2848641
Angus Lei <alei@sfu.ca>
2019-07-15 16:16:01 -0700

    Position interviews in calendar grid, fix toggle class names

--- js/main.js
+++ js/main.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:8c88e01f6cff3045e4d073060a53562ce86a2cc6e1718b52098fe639e4f0903e
-size 6537
+oid sha256:368b756ddaeb682e85011a81a67cb037bf7a902ee3041d163538d20c5e983a6e
+size 6532
