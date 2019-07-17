15ec0786b527764e51f168ef0f8139a2b18fb8ff
Angus Lei <alei@sfu.ca>
2019-07-17 16:52:40 -0700

    Add Redaction typeface

--- js/main.js
+++ js/main.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:4bc39e097d54ffd8e30738928e658a462d5c8b2db148d0ba31d01f0ef4b9a235
-size 7883
+oid sha256:9161253aee57d6034cb90d96258e3bc73db45c0b9dbe60ec29af3f07dbc3b0aa
+size 7878
