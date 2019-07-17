a43c418194d5f1d1555477662b45e873ab065a07
SANI <sanii.trinh@gmail.com>
2019-07-17 13:13:26 -0700

    seperate archive js files

---
+++ js/archive.js
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:bdda8cc70ad21fd38f5effbf5ee9b3ce18412870a8dae76472aab52ec6774dcf
+size 3226
--- js/main.js
+++ js/main.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:3fbfb13c1656d661859f2b90dfc1e534891d40ce0f9f8f57d79949db166176ee
-size 5559
+oid sha256:2d2088c85c5ba02374affd6a430619be4479ac29621e2beceaa8c2231c4dd571
+size 2207
