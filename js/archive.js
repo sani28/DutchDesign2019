1ec24f85b770aba1418641afc1341676f1710b08
Angus Lei <alei@sfu.ca>
2019-07-18 16:30:53 -0700

    We structured everything

--- js/archive.js
+++ js/archive.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:bdda8cc70ad21fd38f5effbf5ee9b3ce18412870a8dae76472aab52ec6774dcf
-size 3226
+oid sha256:8f411c6b241991c23b40a4b2624bc89f6038906e842374344ed1768aec32fc93
+size 3236
