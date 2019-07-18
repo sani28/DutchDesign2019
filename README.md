d0601a71f55f6b458e522f969f396162126be158
SANI <sanii.trinh@gmail.com>
2019-07-18 03:52:47 -0700

    sassy time with reorganized file architecture

--- README.md
+++
@@ -1,3 +0,0 @@
-# DutchDesign2019
-
-In this complex age, we embrace ambiguity and contradiction. dutchDesign is an ongoing platform for field research, observation, and reportage. It attempts to make the naturally rich life of Dutch culture fleetingly visible while leaving a space for it to describe itself. The protagonists of dutchDesign are individuals who inhabit and define their own worlds; who habitually blur and destroy boundaries, definitions, and limitations.
---
+++ js/legacy.js
@@ -0,0 +1,107 @@
+$(document).ready(function() {
+
+var targetDivs = document.querySelectorAll('.archive-stack');
+
+for (var i = 0; i < targetDivs.length; i++) {
+
+  $("#ten").hover(function() {
+      $("#ten").css("background-image", "url(./assets/2010.png)");
+      $("#yearten").css("color", "white");
+    },
+    function() {
+      $("#ten").css("background-image", "none");
+      $("#yearten").css("color", "#7D246B");
+    });
+
+  $("#eleven").hover(function() {
+      $("#eleven").css("background-image", "url(./assets/2011.png)");
+      $("#yeareleven").css("color", "white");
+    },
+    function() {
+      $("#eleven").css("background-image", "none");
+      $("#yeareleven").css("color", "#7D246B");
+    });
+
+  $("#twelve").hover(function() {
+      $("#twelve").css("background-image", "url(./assets/2012.png)");
+      $("#yeartwelve").css("color", "black");
+    },
+    function() {
+      $("#twelve").css("background-image", "none");
+      $("#yeartwelve").css("color", "#7D246B");
+    });
+
+  $("#thirteen").hover(function() {
+      $("#thirteen").css("background-image", "url(./assets/2013.png)");
+      $("#yearthirteen").css("color", "white");
+    },
+    function() {
+      $("#thirteen").css("background-image", "none");
+      $("#yearthirteen").css("color", "#7D246B");
+    });
+
+  $("#fourteen").hover(function() {
+      $("#fourteen").css("background-image", "url(./assets/2014.png)");
+      $("#yearfourteen").css("color", "white");
+    },
+    function() {
+      $("#fourteen").css("background-image", "none");
+      $("#yearfourteen").css("color", "#7D246B");
+    });
+
+  $("#fifteen").hover(function() {
+      $("#fifteen").css("background-image", "url(./assets/2015.png)");
+      $("#yearfifteen").css("color", "white");
+    },
+    function() {
+      $("#fifteen").css("background-image", "none");
+      $("#yearfifteen").css("color", "#7D246B");
+    });
+
+  $("#sixteen").hover(function() {
+      $("#sixteen").css("background-image", "url(./assets/2016.png)");
+      $("#yearsixteen").css("color", "#4cb883");
+    },
+    function() {
+      $("#sixteen").css("background-image", "none");
+      $("#yearsixteen").css("color", "#7D246B");
+    });
+
+  $("#seventeen").hover(function() {
+      $("#seventeen").css("background-image", "url(./assets/2017.svg)");
+      $("#yearseventeen").css("color", "black");
+    },
+    function() {
+      $("#seventeen").css("background-image", "none");
+      $("#yearseventeen").css("color", "#7D246B");
+    });
+
+  $("#eighteen").hover(function() {
+      $("#eighteen").css("background-image", "url(./assets/2018.png)");
+      $("#yeareighteen").css("color", "white");
+    },
+    function() {
+      $("#eighteen").css("background-image", "none");
+      $("#yeareighteen").css("color", "#7D246B");
+    });
+
+  $("#nineteen").hover(function() {
+      $("#nineteen").css("background-image", "url(./assets/2019.png)");
+    },
+    function() {
+      $("#nineteen").css("background-image", "none");
+    });
+
+}; // End of Archive
+
+
+  // Vertical -> Horizontal Scroll Mapping
+  $(function() {
+
+     $("html").mousewheel(function(event, delta) {
+        this.scrollLeft -= (delta * 1);
+     });
+
+  });
+
+});
