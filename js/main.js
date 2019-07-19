7553a6850b577a08f9fe76beaf5a7c160992a2d7
Angus Lei <alei@sfu.ca>
2019-07-19 11:15:09 -0700

    Access dataset from hovering

--- js/main.js
+++ js/main.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:071f6bf199390e9267dea01fc43dce4137d01a199c228415b42b4aa2d0c5ceb5
-size 5769
+oid sha256:b6caeb406b91e9c537eb80f15f2f93a0a46e9bdbdd3664fd6a9ccd4e946f6626
+size 6171
