8d7f42fd18fa733db21fad027fba3061b745d4e7
Angus Lei <alei@sfu.ca>
2019-07-19 12:27:28 -0700

    Hovering behaviour for title/subtitle of dots (sans positioning)

--- js/main.js
+++ js/main.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b6caeb406b91e9c537eb80f15f2f93a0a46e9bdbdd3664fd6a9ccd4e946f6626
-size 6171
+oid sha256:c61f5b520ed958955806f85ca6cc033741d0bfd995dba27774b8f274cf9a3ece
+size 6232
