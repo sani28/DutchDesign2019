a9547d2bf8ba1038e63b0ee6863b7492539ffe67
Angus Lei <alei@sfu.ca>
2019-07-19 15:52:18 -0700

    Added background grid on hover

--- js/main.js
+++ js/main.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:439265bf6e1c7bbfe1c9dc6a0486a1253dc657a66a5f46ec87c322effa78c539
-size 6470
+oid sha256:23ea4afd65470bd7304924e1aa03a9f941eb7ca9fb48fb9d4e0b463201d96379
+size 6558
