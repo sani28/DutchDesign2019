18e4ef7d55df87818a715a8496408a01580158f6
Angus Lei <alei@sfu.ca>
2019-07-19 13:17:16 -0700

    Header text appears above specific dot (hacky)

--- js/main.js
+++ js/main.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c61f5b520ed958955806f85ca6cc033741d0bfd995dba27774b8f274cf9a3ece
-size 6232
+oid sha256:9136a6bcd1b7940bf3b51957baf1fb1c1be6588c1fad7925b4375d891d15f428
+size 6448
