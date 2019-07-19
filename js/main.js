5515ccddd76791b1dcc0a78dde93dec052a5ac09
Angus Lei <alei@sfu.ca>
2019-07-18 18:28:56 -0700

    Changing image background behaviour added for arnhem

--- js/main.js
+++ js/main.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:dc3a27ab28d7af69425ee3ce9290100eb9bf7bd6e0d27a3863ae1d20b4af4472
-size 7570
+oid sha256:152820e7d2c3e8a200b9203b8f926ed9baf4f641241fdd25073b1bf0742dc64c
+size 5874
