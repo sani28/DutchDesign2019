0fc3d97d533cdf494e06e6aacac5964d1be995d2
Angus Lei <alei@sfu.ca>
2019-07-23 15:17:59 -0700

    add play pause hover icons and scroll functions

--- js/designer.js
+++ js/designer.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:6ec1c3f614f2a14a77bfaea9e2964d95043d5571e8e3b537ddd28794cbc310a4
-size 385
+oid sha256:a9bd1458232d5550a28fc7b35b0bf2ce9585891470cc838fae19cddbe97fbd98
+size 2534
