3a2ad37ecded207333569beea7206d2a0bba785e
Angus Lei <alei@sfu.ca>
2019-07-24 16:58:04 -0700

    Successful ScrollMagic integration and reveal image on scroll

--- js/designer.js
+++ js/designer.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:505c1a94eea9dec4df9d7743e05d76dfd8e2e214cb53b2a9a33052ee6d1b3c91
-size 2712
+oid sha256:7f2ec964da1cfa46975273d73f98dc20318265f0496e7e8d54eac4d134502a1f
+size 3765
