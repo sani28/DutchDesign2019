e8a26fb9798e74c6e24c95176765861ae3f04d3c
Angus Lei <alei@sfu.ca>
2019-07-24 18:06:41 -0700

    Resaving LFS pointer files

--- js/main.js
+++ js/main.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:fad26b2e99d04b540adfa14fc6614c6f11fc08f9ed73792c814213c64b44c863
-size 6335
+oid sha256:472d89c077a44b607afe2a09b2a0a5210bcdfa7866804da64fc75a672b48e711
+size 6334
