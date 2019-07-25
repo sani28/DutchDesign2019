df48e389aed59fb30f014ccaeedb9904556aa534
Angus Lei <alei@sfu.ca>
2019-07-24 19:16:46 -0700

    Removed old designer scroll code

--- js/designer.js
+++ js/designer.js
@@ -1,3 +1,54 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:7f2ec964da1cfa46975273d73f98dc20318265f0496e7e8d54eac4d134502a1f
-size 3765
+$(document).ready(function(){
+  const vid = document.getElementById("designer-vid");
+  const overlay = document.getElementById("designer-overlay");
+  const vidSection = document.getElementById("top-video-section");
+  const orderedDesignerImages =
+  [document.getElementById("floris-candid2"), document.getElementById("floris-candid1"),  document.getElementById("floris-candid3"),
+  document.getElementById('floris-quote1'), document.getElementById("floris-workshop"), document.getElementById("floris-space")];
+
+  var scrollControl = new ScrollMagic.Controller();
+  for(let i=0; i<orderedDesignerImages.length; i++){
+    new ScrollMagic.Scene({
+      triggerElement: orderedDesignerImages[i],
+      triggerHook: 0.8,
+      offset: 100,
+      reverse: true
+    })
+    .setClassToggle(orderedDesignerImages[i], "visible")
+    // .addIndicators({name: "image " + (i + 1)}) //USE FOR DEBUG
+    .addTo(scrollControl);
+  }
+  new ScrollMagic.Scene({
+    triggerElement:  "#floris-wip",
+    triggerHook: 0.8,
+    offset: 150,
+    reverse: true
+  })
+  .setClassToggle("#floris-wip", "visible")
+  .addTo(scrollControl);
+
+
+  //VIDEO OVERLAY SHOW/HIDE
+  vid.addEventListener("play", function(){
+    overlay.style.display = "none";
+    $("#top-video-section").removeClass("play-cursor");
+    $("#top-video-section").addClass("pause-cursor");
+  });
+
+  vid.addEventListener("pause", function(){
+    overlay.style.display = "";
+    $("#top-video-section").removeClass("pause-cursor");
+    $("#top-video-section").addClass("play-cursor");
+  });
+
+  vid.addEventListener("playing", function(){
+    $("#top-video-section").removeClass("play-cursor");
+    $("#top-video-section").addClass("pause-cursor");
+  });
+
+  vid.addEventListener("paused", function(){
+    $("#top-video-section").removeClass("pause-cursor");
+    $("#top-video-section").addClass("play-cursor");
+  });
+
+});
