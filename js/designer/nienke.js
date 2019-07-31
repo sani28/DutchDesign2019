35c215b50350f5c739a3201a11bb0fcae72e6ea1
Angus Lei <angusleikh@gmail.com>
2019-07-31 13:53:52 -0700

    Finished nienke page layout with images

--- js/designer/nienke.js
+++ js/designer/nienke.js
@@ -1,4 +1,35 @@
 $(document).ready(function(){
-  const orderedDesignerImages = [];
-  
+  const orderedDesignerImages =
+  [document.getElementById('nienkecandid'),
+   document.getElementById('nienkecandid2'),
+   document.getElementById('nienkecandid3'),
+   document.getElementById('quote1'),
+   document.getElementById('netting'),
+   document.getElementById('samples'),
+   document.getElementById('nienkequote'),
+   document.getElementById('mournsmall'),
+   document.getElementById('mourn')];
+
+   var scrollControl = new ScrollMagic.Controller();
+
+   for(let i=0; i<orderedDesignerImages.length; i++){
+     new ScrollMagic.Scene({
+       triggerElement: orderedDesignerImages[i],
+       triggerHook: 0.8,
+       offset: 100,
+       reverse: true
+     })
+     .setClassToggle(orderedDesignerImages[i], "visible")
+     .addTo(scrollControl);
+   }
+
+   new ScrollMagic.Scene({
+     triggerElement: "#seame",
+     triggerHook: 0.8,
+     offset: 300,
+     reverse: true
+   })
+   .setClassToggle("#seame", "visible")
+   .addTo(scrollControl);
+
 });
