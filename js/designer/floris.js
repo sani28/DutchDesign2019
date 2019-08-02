a07ee0248f27047f6d28fe7a0f0f099cdc79570d
Angus Lei <angusleikh@gmail.com>
2019-08-02 16:56:22 -0700

    Renamed folder and added images/layout for truly truly

--- js/designer/floris.js
+++ js/designer/floris.js
@@ -13,29 +13,6 @@ $(document).ready(function(){
   document.getElementById("compmap"),
   document.getElementById("quote4")];
 
-  const scrollProg = document.getElementById("scroll-progress");
-  var controller = new ScrollMagic.Controller();
-  var progController = new ScrollMagic.Controller();
-  var totalHeight = document.body.clientHeight;
-
-  function pathPrepare ($el) {
-    let lineLength = $el.getTotalLength();
-    $el.style.strokeDasharray = lineLength;
-    $el.style.strokeDashoffset = lineLength;
-  }
-  // prepare SVG
-  pathPrepare(scrollProg);
-  var tween = new TimelineLite()
-    .add(TweenLite.to(scrollProg, 1.0, {strokeDashoffset: 0, ease:Linear.easeNone}));
-
-  new ScrollMagic.Scene({
-          duration: totalHeight - 900,
-          offset: 175,
-          tweenChanges: true
-        })
-        .setTween(tween)
-        .addTo(controller);
-  
   var scrollControl = new ScrollMagic.Controller();
   for(let i=0; i<orderedDesignerImages.length; i++){
     new ScrollMagic.Scene({
