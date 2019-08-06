a4f9e3ccc3f694d19c428f78c743eaf895d3c5b9
SANI <sanii.trinh@gmail.com>
2019-08-06 16:30:12 -0700

    cursor+fade in blurb mergge

--- js/main.js
+++ js/main.js
@@ -1,6 +1,38 @@
 
 $(document).ready(function(){
 
+
+
+    //CURSOR
+      const $bigBall = document.querySelector('.cursor__ball--big');
+      const $smallBall = document.querySelector('.cursor__ball--small');
+      const $hoverables = document.querySelectorAll('.hoverable');
+
+      // Listeners
+      document.body.addEventListener('mousemove', onMouseMove);
+      for (let i = 0; i < $hoverables.length; i++) {
+        $hoverables[i].addEventListener('mouseenter', onMouseHover);
+        $hoverables[i].addEventListener('mouseleave', onMouseHoverOut);
+      }
+
+      // Move the cursor
+      function onMouseMove(e) {
+        TweenMax.to($smallBall, .1, {
+          x: e.pageX - 5,
+          y: e.pageY - 7 });
+
+      }
+      // Hover an element
+      function onMouseHover() {
+        TweenMax.to($smallBall, .3, {
+          scale: 4 });
+
+      }
+      function onMouseHoverOut() {
+        TweenMax.to($smallBall, .3, {
+          scale: 1 });
+      }
+
   //ELEMENTS
   const designerDots = $(".designer-dot").get();
   const experienceDots = $(".experience-dot").get();
@@ -225,5 +257,6 @@ $(document).ready(function(){
       }
     });
   }
+
 
 }); //DOCREADY DON'T DELETE
