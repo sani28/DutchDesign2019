cbb94fa1fca2ff6223ce48301e3035f03453b1f7
Angus Lei <angusleikh@gmail.com>
2019-08-07 23:58:48 -0700

    Adding momkai page, uploaded IF, SM and Momkai img/gifs

---
+++ js/designer/momkai.js
@@ -0,0 +1,19 @@
+$(document).ready(function(){
+  const orderedDesignerImages =
+  [document.getElementById('harald1'),
+   document.getElementById('harald2'),
+];
+
+  var scrollControl = new ScrollMagic.Controller();
+
+  for(let i=0; i<orderedDesignerImages.length; i++){
+    new ScrollMagic.Scene({
+      triggerElement: orderedDesignerImages[i],
+      triggerHook: 0.8,
+      offset: 100,
+      reverse: true
+    })
+    .setClassToggle(orderedDesignerImages[i], "visible")
+    .addTo(scrollControl);
+  }
+});
