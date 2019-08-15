2ad2463ef74df42155c30759f932e1ba2b17aa6f
SANI <sanii.trinh@gmail.com>
2019-08-15 15:09:42 -0700

    positioned background elements

--- js/main.js
+++ js/main.js
@@ -268,8 +268,8 @@ $(window).load(function() {
     direction: 'up',
     loop:-1,
     scrolldelay: 0,
-    scrollamount:90,
-    circular: false,
-    drag: false
+    scrollamount: 90,
+    inverthover: false,
+    hoverstop: false,
   });
 })
