b958d3b8c17b686ee6967cafe091706b3f6163aa
Angus Lei <angusleikh@gmail.com>
2019-09-07 21:34:33 -0700

    Cursor fixes to designer pages

--- js/wid.js
+++ js/wid.js
@@ -1,4 +1,28 @@
 $(document).ready(function() {
+  const $bigBall = document.querySelector('.cursor__ball--big');
+  const $smallBall = document.querySelector('.cursor__ball--small');
+  var $hoverables = document.getElementsByClassName('.hoverable');
+
+  for (let i = 0; i < $hoverables.length; i++) {
+    $hoverables[i].addEventListener('mouseenter', onMouseHover);
+    $hoverables[i].addEventListener('mouseleave', onMouseHoverOut);
+  }
+
+  // Move the cursor
+  function onMouseMove(e) {
+    TweenMax.to($smallBall, .1, {
+      x: e.pageX - 5,
+      y: e.pageY - 7 });
+  }
+  // Hover an element
+  function onMouseHover() {
+    TweenMax.to($smallBall, .3, {
+      scale: 4 });
+  }
+  function onMouseHoverOut() {
+    TweenMax.to($smallBall, .3, {
+      scale: 1 });
+  }
 
   function instantTooltips(textFrom, delta) {
     delta = parseFloat(delta) ? parseFloat(delta) : 5;
