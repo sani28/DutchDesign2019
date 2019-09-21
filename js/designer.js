e676f270b221536ea79fe20d8a6d2056a8e527c6
Angus Lei <angus@d142-058-219-111.wireless.sfu.ca>
2019-09-20 17:55:07 -0700

    New Green, who dis?

--- js/designer.js
+++ js/designer.js
@@ -13,7 +13,7 @@ $(document).ready(function() {
   var scrollControl = new ScrollMagic.Controller();
   var jumpController = new ScrollMagic.Controller();
   var progController = new ScrollMagic.Controller();
-  var totalHeight = $(document).height();
+  var totalHeight = $(document).height() - 700;
 
   var revertProgress = TweenLite.from("#animate", 0.5, {
     autoAlpha: 0,
@@ -42,7 +42,7 @@ $(document).ready(function() {
     .addTo(scrollController);
 
   var fillScrollProgress = new ScrollMagic.Scene({
-      duration: totalHeight - 200,
+      duration: totalHeight,
       offset: 175,
       tweenChanges: true
     })
