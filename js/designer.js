49265b59bfe660b1d9fd3218685ac61e86ce2bf5
Angus Lei <angus@d142-058-219-111.wireless.sfu.ca>
2019-09-20 19:26:50 -0700

    Right before upload?!

--- js/designer.js
+++ js/designer.js
@@ -13,7 +13,7 @@ $(document).ready(function() {
   var scrollControl = new ScrollMagic.Controller();
   var jumpController = new ScrollMagic.Controller();
   var progController = new ScrollMagic.Controller();
-  var totalHeight = $(document).height() - 1000;
+  var totalHeight = $(document).height() - 800;
 
   var revertProgress = TweenLite.from("#animate", 0.5, {
     autoAlpha: 0,
